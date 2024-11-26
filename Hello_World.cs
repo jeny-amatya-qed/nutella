59e1cbccbfdca28c6cc1e68d2d1d1d2199537c7b
jeny-amatya-qed <134246342+jeny-amatya-qed@users.noreply.github.com>
2024-11-27 08:43:46 +1000

    Update Hello_World.cs

--- Hello_World.cs
+++ Hello_World.cs
@@ -1,3 +1,4 @@
+// $livedoc$ This is an important entry point to the application. Anyone learning a new programming language should surely "Hello World" first before anything else!
 Console.WriteLine("Hello World!");
 
 Console.WriteLine("Hello World!");
